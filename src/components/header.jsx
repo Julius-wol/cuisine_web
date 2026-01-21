93781fdf4c2d85d092c9521eb3a5a483c5bc9343
khoa2501 <anhkhoa20052501@gmail.com>
2026-01-21 11:27:32 +0700

    header

---
+++ src/components/header.jsx
@@ -0,0 +1,34 @@
+import logo from '../assets/logo/Logo.PNG';
+import './header.css';
+
+function Header() {
+    return (
+        <>
+            <div className="header">
+                <div className="logo">
+                    <a href="/"><img src={logo} alt="Logo" /></a>
+                </div>
+
+                <div className="header-content">
+                    <form>
+                        <input type="text" placeholder="Search in website" />
+                        <button type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+
+            <div className="nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default Header;
