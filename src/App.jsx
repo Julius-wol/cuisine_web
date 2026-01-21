0b18394704fea39f76be5282e6250ca892b38d61
Lê Đăng Trung Hiếu <hieuldt.dev@gmail.com>
2026-01-21 10:56:17 +0700

    Add Cart Product

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CartProduct from './components/cartProduct'   // 👈 import component
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState([])
+
+  const handleAddToCart = (item) => {
+    setCart((prev) => [...prev, item])
+    console.log('Cart:', item)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* GỌI COMPONENT CART PRODUCT */}
+      <CartProduct onAddToCart={handleAddToCart} />
+
+      {/* HIỂN THỊ CART (test nhanh) */}
+
     </>
   )
 }
